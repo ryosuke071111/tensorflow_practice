00d029722bc4260d39f3501ff5194acef9a0bf6a
桑原亮介 <ryosuke0711993@gmail.com>
2018-08-31 14:45:50 +0900

    done with prologue

---
+++ modularity.py
@@ -0,0 +1,68 @@
+from utility import *
+
+reset_graph()
+
+# × 冗長
+#--------------------------------------------------------------
+n_features = 3
+X = tf.placeholder(tf.float32, shape=(None, n_features), name="X")
+
+w1 = tf.Variable(tf.random_normal((n_features, 1)), name="weights1")
+w2 = tf.Variable(tf.random_normal((n_features, 1)), name="weights2")
+b1 = tf.Variable(0.0, name="bias1")
+b2 = tf.Variable(0.0, name="bias2")
+
+z1 = tf.add(tf.matmul(X, w1), b1, name="z1")
+z2 = tf.add(tf.matmul(X, w2), b2, name="z2")
+
+relu1 = tf.maximum(z1, 0., name="relu1")
+relu2 = tf.maximum(z1, 0., name="relu2")  # Oops, cut&paste error! Did you spot it?
+
+output = tf.add(relu1, relu2, name="output")
+#--------------------------------------------------------------
+
+
+
+# △ 改善できる
+#--------------------------------------------------------------
+reset_graph()
+
+def relu(X):
+    w_shape = (int(X.get_shape()[1]), 1)
+    w = tf.Variable(tf.random_normal(w_shape), name="weights")
+    b = tf.Variable(0.0, name="bias")
+    z = tf.add(tf.matmul(X, w), b, name="z")
+    return tf.maximum(z, 0., name="relu")
+
+n_features = 3
+X = tf.placeholder(tf.float32, shape=(None, n_features), name="X")
+relus = [relu(X) for i in range(5)]
+output = tf.add_n(relus, name="output")
+output = tf.add(relu1, relu2, name="output")
+#--------------------------------------------------------------
+
+
+
+
+# ○ 改善できる
+#--------------------------------------------------------------
+def relu(X):
+    with tf.name_scope("relu"):
+        w_shape = (int(X.get_shape()[1]), 1)                          # not shown in the book
+        w = tf.Variable(tf.random_normal(w_shape), name="weights")    # not shown
+        b = tf.Variable(0.0, name="bias")                             # not shown
+        z = tf.add(tf.matmul(X, w), b, name="z")                      # not shown
+        return tf.maximum(z, 0., name="max")
+#--------------------------------------------------------------
+
+n_features = 3
+X = tf.placeholder(tf.float32, shape=(None, n_features), name="X")
+relus = [relu(X) for i in range(5)]
+output = tf.add_n(relus, name="output")
+
+file_writer = tf.summary.FileWriter("logs/relu2", tf.get_default_graph())
+file_writer.close()
+
+
+
+
--- tensorboard.py
+++ tensorboard.py
@@ -1,12 +1,11 @@
-from utility import *
 from sklearn.datasets import fetch_california_housing
 import numpy as np
 import tensorflow as tf
 from sklearn.preprocessing import StandardScaler
+from utility import *
 
 #TensorBoard用
 #-------------------------------------------------
-from utility import *
 from datetime import datetime
 
 now = datetime.utcnow().strftime("%Y%m%d%H%M%S")
@@ -15,22 +14,25 @@ logdir = "{}/run - {}/".format(root_logdir, now)
 #--------------------------------------------------
 
 n_epochs = 1000
-learning_rate = 0.02
+learning_rate = 0.01
 
-reset_graph()
 
 X = tf.placeholder(tf.float32, shape=(None, n+1), name = "X")
 y = tf.placeholder(tf.float32, shape=(None, 1), name = "y")
 
 theta = tf.Variable(tf.random_uniform([n+1, n], -1.0, 1.0, seed = 42), name ="theta")
 y_pred = tf.matmul(X, theta, name = "predictions")
-error = y_pred - y
-mse = tf.reduce_mean(tf.square(error), name = "mse")
+
+with tf.name_scope("loss") as scope:
+  error = y_pred - y
+  mse = tf.reduce_mean(tf.square(error), name = "mse")
+
+
+
 optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
 training_op = optimizer.minimize(mse)
 
 init = tf.global_variables_initializer()
-saver = tf.train.Saver()
 
 #TensorBard用
 #------------------------------------------------------------------
@@ -49,28 +51,14 @@ with tf.Session() as sess:
   for epoch in range(n_epochs):
     for batch_index in range(n_batchs):
       X_batch, y_batch = fetch_batch(epoch, batch_index, batch_index, n_batchs)
+      if batch_index % 10 == 0:
+        summary_str = mse_summary.eval(feed_dict={X: X_batch, y:y_batch})
+        step = epoch * n_batchs + batch_index
+        file_writer.add_summary(summary_str, step)
       sess.run(training_op, feed_dict={X:X_batch, y:y_batch})
+
   best_theta = theta.eval()
-  print(best_theta)
-  save_path = saver.save(sess, "/Users/ryousuke/desktop/ml/tensorflow_practice/training_data/my_model_final.ckpt")
 
-## モデルの復元
-# with tf.Session()as sess:
-#   saver.restore(sess, "/Users/ryousuke/desktop/ml/tensorflow_practice/training_data/my_model_final.ckpt")
-#   best_theta_restored = theta.eval()
-# print(best_theta_restored)
 
-# np.allclose(best_theta, best_theta_restored)
+file_writer.close()
 
-# ## 特定の変数のみ保存
-# saver = tf.train.Saver({"weights":theta})
-
-# ## グラフ構造の読み込み
-# saver = tf.train.import_meta_graph("/Users/ryousuke/desktop/ml/tensorflow_practice/training_data/my_model_final.ckpt.meta")
-# ## グラフ構造を読み込まれたものに対してパラメータ呼び出し
-# with  tf.Session()as sess:
-#   saver.restore(sess, "/Users/ryousuke/desktop/ml/tensorflow_practice/training_data/my_model_final.ckpt")
-#   best_theta_restored = theta.eval()
-
-# print(best_theta_restored)
-# np.allclose(best_theta, best_theta_restored)
